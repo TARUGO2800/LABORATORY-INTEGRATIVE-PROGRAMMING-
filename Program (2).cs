f8a5ee00b201410e4f06fa9306c9d3865689ca97
TARUGO2800 <advgamings290@gmail.com>
2026-02-07 14:34:54 +0800

    Add files via upload

---
+++ Program (2).cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MathApp
+{
+    class MathProgram
+    {
+        static void Main(string[] args)
+        {
+            int a = 11;
+            int b = 5;
+
+            int absoluteValue = Math.Abs(-a);
+
+            int maxValue = Math.Max(a, b);
+
+            int minValue = Math.Min(a, b);
+
+            double powerValue = Math.Pow(a, b);
+
+            double squareRootValue = Math.Sqrt(a);
+
+            Console.WriteLine("------------------");
+            Console.WriteLine("Absolute value: " + absoluteValue);
+            Console.WriteLine("Maximum value: " + maxValue);
+            Console.WriteLine("Minimum value: " + minValue);
+            Console.WriteLine("Power value: " + powerValue);
+            Console.WriteLine("Square root: " + squareRootValue);
+            Console.WriteLine("------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
